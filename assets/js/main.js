2251c264253ae72a762ca8cbc29eadc8d332d58b
Erick Díaz <82739201+erickdc7@users.noreply.github.com>
2024-07-26 19:17:21 -0500

    add show menu function

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,21 @@
 /*=============== SHOW MENU ===============*/
+const navMenu = document.getElementById('nav-menu'),
+    navToggle = document.getElementById('nav-toggle'),
+    navClose = document.getElementById('nav-close')
 
+/* Menu show */
+if (navToggle) {
+    navToggle.addEventListener('click', () => {
+        navMenu.classList.add('show-menu')
+    })
+}
+
+/* Menu hidden */
+if (navClose) {
+    navClose.addEventListener('click', () => {
+        navMenu.classList.remove('show-menu')
+    })
+}
 
 /*=============== REMOVE MENU MOBILE ===============*/
 
@@ -7,10 +23,10 @@
 /*=============== ADD BLUR HEADER ===============*/
 
 
-/*=============== SWIPER PLANETS ===============*/ 
+/*=============== SWIPER PLANETS ===============*/
 
 
-/*=============== SHOW SCROLL UP ===============*/ 
+/*=============== SHOW SCROLL UP ===============*/
 
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
