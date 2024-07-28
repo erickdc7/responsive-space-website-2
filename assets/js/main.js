3d1cd677bcca0b7003dedd584c86e07fe80b1ce1
Erick Díaz <82739201+erickdc7@users.noreply.github.com>
2024-07-28 12:01:21 -0500

    add scroll up function

--- assets/js/main.js
+++ assets/js/main.js
@@ -61,7 +61,14 @@ const swiperTravel = new Swiper('.travel__swiper', {
 });
 
 /*=============== SHOW SCROLL UP ===============*/
-
+const scrollUp = () => {
+    const scrollUp = document.getElementById('scroll-up')
+    // When the scroll is higher than 350 viewport height, add the show-scroll class to the a tag with the scrollup class
+    this.scrollY >= 350
+        ? scrollUp.classList.add('show-scroll')
+        : scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
 
